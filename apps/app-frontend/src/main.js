95e4c89d2d1f0cc29959b5fec53594869132201f
成成0v0 <chengcheng@miao.ms>
2025-06-01 00:14:55 +0800

    refactor: 移除 Sentry 追踪器

--- apps/app-frontend/src/main.js
+++ apps/app-frontend/src/main.js
@@ -6,7 +6,6 @@ import { createPinia } from 'pinia'
 import FloatingVue from 'floating-vue'
 import 'floating-vue/dist/style.css'
 import { createPlugin } from '@vintl/vintl/plugin'
-import * as Sentry from '@sentry/vue'
 
 const VIntlPlugin = createPlugin({
   controllerOpts: {
@@ -29,12 +28,12 @@ const pinia = createPinia()
 
 let app = createApp(App)
 
-Sentry.init({
-  app,
-  dsn: 'https://9508775ee5034536bc70433f5f531dd4@o485889.ingest.us.sentry.io/4504579615227904',
-  integrations: [Sentry.browserTracingIntegration({ router })],
-  tracesSampleRate: 0.1,
-})
+// Sentry.init({
+//   app,
+//   dsn: 'https://9508775ee5034536bc70433f5f531dd4@o485889.ingest.us.sentry.io/4504579615227904',
+//   integrations: [Sentry.browserTracingIntegration({ router })],
+//   tracesSampleRate: 0.1,
+// })
 
 app.use(router)
 app.use(pinia)
