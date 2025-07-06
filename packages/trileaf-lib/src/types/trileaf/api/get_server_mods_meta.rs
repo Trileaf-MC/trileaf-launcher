fe1e05d0dbaa46b907e0c70c62d9bcf6ac3e0426
成成0v0 <chengcheng@miao.ms>
2025-07-06 17:17:08 +0800

    feat(trileaf-lib): add type definition for API response

--- packages/trileaf-lib/src/types/trileaf/api/get_server_mods_meta.rs
+++ packages/trileaf-lib/src/types/trileaf/api/get_server_mods_meta.rs
@@ -1,19 +1,26 @@
+use crate::utils;
+use serde::{Deserialize, Serialize};
+
 use crate::types::{
     sha::Sha512Hash, trileaf::minecraft_mod_meta::TrileafMinecraftModSource,
 };
 
 /// https://apifox.com/apidoc/shared/0590d539-cd25-46ea-b26f-96ca50ef7aad/api-310438915
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct RequestPathParams {
     pub server_id: u64,
 }
 
 /// https://apifox.com/apidoc/shared/0590d539-cd25-46ea-b26f-96ca50ef7aad/api-310438915
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct ResponsePayload {
     pub source: TrileafMinecraftModSource,
     pub content: ResponsePayloadContent,
 }
 /// A part of `ResponsePayload`.
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct ResponsePayloadContent {
+    #[serde(with = "utils::serde::serde_sha512_as_hex")]
     pub hash: Sha512Hash,
     pub version: String,
 }
--- packages/trileaf-lib/src/types/trileaf/api/mod.rs
+++ packages/trileaf-lib/src/types/trileaf/api/mod.rs
@@ -1,1 +1,11 @@
+use serde::{Deserialize, Serialize};
+
 pub mod get_server_mods_meta;
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct ApiResponse<T> {
+    pub success: bool,
+    pub code: String, // TODO: 计划迁移至 `ErrorCode` 以获得更精确的错误处理
+    pub message: String,
+    pub payload: T,
+}
